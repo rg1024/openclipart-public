c21380d2e6dcf54e96aad9dea855fca66b69800d
Jon Phillips <jon@fabricatorz.com>
2013-08-30 19:59:33 +0800

    made the script better to upload clipart

--- bin/openclipart-upload.sh
+++ bin/openclipart-upload.sh
@@ -6,35 +6,60 @@
 # clipart files...but sure, I like the stats for my account to go up. I can
 # take credit for your work sure :)
 
+USAGE="$0 \"Some Title\" \"Some Summary\" \"./my-file.pdf\" \"from@email.com\""
+
+DEBUG=0
+
 FROM=jon@rejon.org
 TO=upload@openclipart.org
+# TO=jon@rejon.org
 
 FROM_NAME="Jon Phillips"
-HEADER="my_hdr From:$EMAIL;"
-
-if [ -n "$4" ]; then TO="$4"; fi
-# if [ -n "$5" ]; then FROM_NAME="$5"; fi
-
-TITLE="$1"
-SUMMARY="$2"
-FILEPATH="$3"
 
 
-if [ -n "$1" ]; then TITLE="$1"; else exit 1; fi
+#TITLE="$1"
+#SUMMARY="$2"
+#FILE="$3"
+
+
+if [ -n "$1" ]; 
+then :
+    TITLE="$1"; 
+else : 
+    echo "$USAGE"
+    exit 1; 
+fi
+
 if [ -n "$2" ]; then SUMMARY="$2"; else exit 1; fi
-if [ -n "$3" ]; then FILEPATH="$3"; else exit 1; fi
+if [ -n "$3" ]; 
+then :
+    FILE="$3";
+    # should check here to make sure its a valid file type or error out
+else : 
+    exit 1; 
+fi
 
+if [ -n "$4" ]; then FROM="$4"; fi
+#if [ -n "$5" ]; then FROM_NAME="$5"; fi
 
-echo $FROM
-echo $TO
-echo $TITLE
-echo $SUMMARY
-echo $FILEPATH
-echo ""
+HEADER="my_hdr From:$FROM;"
+
+if [ $DEBUG == 1 ]; 
+then :
+    echo "FROM: $FROM"
+    echo "TO: $TO"
+    echo "TITLE: $TITLE"
+    echo "SUMMARY: $SUMMARY"
+    echo "FILE: $FILE"
+    exit 1
+fi
+
 
 #FILE="IMG_20100818_145428.jpg"
 
-CT=0
-for FILE in $FILEPATH/*.{jpg,JPG,png,PNG,JPEG,jpeg,svg,SVG}; do \
-    echo "$FILE" && echo -e "$SUMMARY" | mutt -e "$HEADER" -a "$FILE" -s "$TITLE $CT" -- "$TO" && sleep 2 && CT=$((CT+1)) ;
-done;
+#CT=0
+#for FILE in $FILEPATH/*.{jpg,JPG,png,PNG,JPEG,jpeg,svg,SVG}; do \
+#    echo "$FILE" && echo -e "$SUMMARY" | mutt -e "$HEADER" -a "$FILE" -s "$TITLE $CT" -- "$FROM_NAME <$TO>" && sleep 2 && CT=$((CT+1)) ;
+#done;
+
+echo -e "$SUMMARY" | mutt -e "$HEADER" -a "$FILE" -s "$TITLE" -- "$TO"
